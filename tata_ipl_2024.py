c8d1feaa8b71a2426f6e928bc31c2c3fd2737c1c
amit redhu <redhus341@gmail.com>
2024-10-25 12:03:27 +0530

    setup the chrome webdriver to access thr web by selenium

--- tata_ipl_2024.py
+++ tata_ipl_2024.py
@@ -5,3 +5,13 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import time
 
+# Website URL and WebDriver path
+website = 'https://www.iplt20.com/stats/2024'
+path = r"C:\Users\redhu\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+s = Service(path)
+
+# Start WebDriver
+driver = webdriver.Chrome(service=s)
+driver.get(website)
+time.sleep(5)
+
