bcf30c5b27e5f40e98cbb0a81c3f6ea64ca247d5
amit redhu <redhus341@gmail.com>
2024-10-25 11:59:57 +0530

    intital commit

---
+++ tata_ipl_2024.py
@@ -0,0 +1,7 @@
+# importing necessary libraries
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+
